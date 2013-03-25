2121a8f4509ae1cce8fc7c781171bc53609792bc
Ryan Cook <cookrn@gmail.com>
2013-03-25 11:24:48 -0600

    ensure that all trial requests are sent to the server

--- libexec/functions/provisioning.bash
+++ libexec/functions/provisioning.bash
@@ -29,21 +29,6 @@ Provisioning...
 #
 provision_service(){
   local service="$1"
-  case $service in
-    redis)
-      provision_redis
-      ;;
-    *)
-      provision_unknown $service
-      ;;
-  esac
-}
-
-# provision_redis
-#
-# Provision a redis instance
-#
-provision_redis(){
   prepare_adv_host
 
   set +e
@@ -54,24 +39,16 @@ provision_redis(){
         -s -L --post301 --post302 \
         -F "user[email]"="$adv_user_email" \
         -F "user[api_key]"="$adv_user_api_key" \
-        -F "instance[service_slug]"="redis" \
+        -F "instance[service_slug]"="$service" \
         "$adv_host/instances" 2>&1
     )
 
   if [[ $? > 0 ]]
   then
-    fail "REDIS: Unable to create an instance at this time!"
+    fail "$service: Unable to create an instance at this time!"
   else
-    echo "REDIS: $ret"
+    echo "$service: $ret"
   fi
 
   set -e
-}
-
-# provision_unknown
-#
-# We don't know how to provision a service named like the argument
-#
-provision_unknown(){
-  echo "We're not able to provision $1 for you quite yet."
 }
