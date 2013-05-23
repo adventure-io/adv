da9bf455b6db6b640a495f65009d0b4425aeac31
Ryan Cook <cookrn@gmail.com>
2013-05-23 16:24:16 -0600

    pass a state with the adv list command

--- libexec/functions/instances/listing.bash
+++ libexec/functions/instances/listing.bash
@@ -9,12 +9,15 @@
 list_instances(){
   prepare_adv_host
 
+  local state=$1
+
   set +e
 
   local ret
   ret=$(
       curl \
         -s -L -G \
+        --data-urlencode "state"="$state" \
         --data-urlencode "user[email]"="$adv_user_email" \
         --data-urlencode "user[api_key]"="$adv_user_api_key" \
         "$adv_host/instances" 2>&1
